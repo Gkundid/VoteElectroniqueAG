b8bcaed45f72435126d42245acb833cd57d94496
Gkundid <114490338+Gkundid@users.noreply.github.com>
2023-04-04 17:20:37 +0200

    wip : add enum Vote and struct resolution and some variables

--- Election.sol
+++ Election.sol
@@ -4,23 +4,48 @@ pragma solidity ^0.4.12;
 
 import "./Ownable.sol";
 import "./SafeMath.sol";
+import "./Whitelist.sol";
 
 contract Election is Ownable {
 
 using SafeMath for uint256;
 
-    // Model a Candidate
-    struct Candidate {
-        uint256 id;
-        string name;
-        uint voteCount;
+    // Votes enum
+    enum vote {POUR, CONTRE, NEUTRE}
+
+    // Vote count to increment for each vote
+    struct resolution{
+        uint id; // identifiant de resolution
+        string titre; 
+        uint pour; // nombre de votes "POUR"
+        uint contre; // nombre de votes "CONTRE"
+        uint neutre; // nombre de votes "NEUTRE"
     }
 
-    // Store accounts that have voted
+    // AG's variable
+    address public presidentDeSeance;
+    address public scrutateur;
+    address public secretaire;
+
+    uint public nombreDeResolutions; // total vote
+
+     // Store accounts that have voted
+    mapping(uint => resolution) public voteCount;
     mapping(address => bool) public voters;
+
+
+    // Model a Candidate
+    // struct Candidate {
+    //     uint256 id;
+    //     string name;
+    //   //  uint voteCount;
+    // }
+
+   
+    
     // Store Candidates
     // Fetch Candidate
-    mapping(uint => Candidate) public candidates;
+    // mapping(uint => Candidate) public candidates;
     // Store Candidates Count
     uint public candidatesCount;
 
@@ -28,24 +53,24 @@ using SafeMath for uint256;
     event votedEvent ( uint indexed _candidateId);
 
     function addCandidate (string memory _name) public {
-        candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        // candidatesCount ++;
+        // candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    function vote (uint _candidateId) public {
+//    function vote (uint _candidateId) public {
         // require that they haven't voted before
-        require(!voters[msg.sender]);
+//        require(!voters[msg.sender]);
 
         // require a valid candidate
-        require(_candidateId > 0 && _candidateId <= candidatesCount);
+//        require(_candidateId > 0 && _candidateId <= candidatesCount);
 
         // record that voter has voted
-        voters[msg.sender] = true;
+//        voters[msg.sender] = true;
 
         // update candidate vote Count
-        candidates[_candidateId].voteCount ++;
+//        candidates[_candidateId].voteCount ++;
 
         // trigger voted event
-        emit votedEvent (_candidateId);
-    }
+//        emit votedEvent (_candidateId);
+//    }
 }
