2f31c42e395484e2afb1f205c4a5d563eaff398b
Gkundid <114490338+Gkundid@users.noreply.github.com>
2023-04-18 15:25:14 +0200

    Add function setEmployes

--- Election.sol
+++ Election.sol
@@ -1,76 +1,94 @@
-pragma solidity ^0.4.12;
-
 // SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.4.12;
 
 import "./Ownable.sol";
 import "./SafeMath.sol";
 import "./Whitelist.sol";
 
-contract Election is Ownable {
+contract Election is Ownable, Whitelist {
 
 using SafeMath for uint256;
 
-    // Votes enum
-    enum vote {POUR, CONTRE, NEUTRE}
+// struct a Candidate
+struct Candidate {
+    uint256 id;
+    string name;
+    uint voteCount;
+    uint forVotes;
+    uint againstVotes;
+    uint neutralVotes;
+}
 
-    // Vote count to increment for each vote
-    struct resolution{
-        uint id; // identifiant de resolution
-        string titre; 
-        uint pour; // nombre de votes "POUR"
-        uint contre; // nombre de votes "CONTRE"
-        uint neutre; // nombre de votes "NEUTRE"
-    }
+// Set a mapping to save accounts that have voted with bool.
+mapping(address => bool) public voters;
 
-    // AG's variable
-    address public presidentDeSeance;
-    address public scrutateur;
-    address public secretaire;
+// Set a mapping to save candidates with their id, name and voteCount.
+mapping(uint => Candidate) public candidates;
 
-    uint public nombreDeResolutions; // total vote
+// Set candidates count.
+uint public candidatesCount;
 
-     // Store accounts that have voted
-    mapping(uint => resolution) public voteCount;
-    mapping(address => bool) public voters;
+//
+address public president;
+address public scrutateur;
+address public secretaire;
 
+// Create an event that will be called when a person votes.
+// (set the candidate id that for who the person voted in parameters)
+event votedEvent ( uint indexed _candidateId);
 
-    // Model a Candidate
-    // struct Candidate {
-    //     uint256 id;
-    //     string name;
-    //   //  uint voteCount;
-    // }
+// Create a function only usable by the owner.
+function addCandidate (string memory _name) public onlyOwner {
+    // Increment variable candidatesCount.
+    candidatesCount ++;
 
-   
+    /* Create new object Candidate with the candidate id (candidatesCount), 
+    *  the candidate's name and the number of votes for this candidates 
+    *  which is initialized to 0 because a new candidate doesn't have vote.
+    */
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0, 0, 0, 0);
+}
+
+// Create a function to vote for a candidate.
+function vote (uint _candidateId, uint _voteType) public {
     
-    // Store Candidates
-    // Fetch Candidate
-    // mapping(uint => Candidate) public candidates;
-    // Store Candidates Count
-    uint public candidatesCount;
+    // Add condition to be sure that a person hasn't already voted.
+    require(!voters[msg.sender], "Already voted!!");
+    // require(!voters[msg.sender] , "Already voted for this candidate!!");
 
-    // voted event
-    event votedEvent ( uint indexed _candidateId);
 
-    function addCandidate (string memory _name) public {
-        // candidatesCount ++;
-        // candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    // Add condition to check the candidate's id. This id must be between 1 and
+    // the candidatesCount number.
+    require(_candidateId > 0 && _candidateId <= candidatesCount, "Error on candidate id.");
+
+    // Check if the voter has already voted for this candidate.
+    
+
+    // Check if the voter is on whitelist.
+    //TODO :
+    require(isWhitelisted(msg.sender), "This person is not on whitelist.");
+
+    // Set that the voter has voted
+    voters[msg.sender] = true;
+
+    // Increment the voteCount for the candidate.
+    candidates[_candidateId].voteCount ++;
+
+    // Increment the 
+    if (_voteType == 1) {
+            candidates[_candidateId].forVotes ++; 
+    } else if (_voteType == 2) {
+            candidates[_candidateId].againstVotes ++;
+    } else if (_voteType == 3) {
+            candidates[_candidateId].neutralVotes ++; 
     }
+}
 
-//    function vote (uint _candidateId) public {
-        // require that they haven't voted before
-//        require(!voters[msg.sender]);
-
-        // require a valid candidate
-//        require(_candidateId > 0 && _candidateId <= candidatesCount);
-
-        // record that voter has voted
-//        voters[msg.sender] = true;
-
-        // update candidate vote Count
-//        candidates[_candidateId].voteCount ++;
-
-        // trigger voted event
-//        emit votedEvent (_candidateId);
-//    }
+    // Set the president, scrutateur and secretaire of the AG.
+    function setEmployes(address _president, address _scrutateur, address _secretaire) public onlyOwner {
+        president = _president;
+        scrutateur = _scrutateur;
+        secretaire = _secretaire;
+    }
 }
